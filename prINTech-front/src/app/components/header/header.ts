3a8e502e38960b35e697ef76b302fedc17dd1260
huet <marie.huet.333@gmail.com>
2026-08-16 17:09:08 +0200

    Affichage de prINTech et de son logo

--- prINTech-front/src/app/components/header/header.ts
+++ prINTech-front/src/app/components/header/header.ts
@@ -1,12 +1,12 @@
 import { Component, inject, signal, computed } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { NgIf } from '@angular/common';
 import { AvatarStateService } from '../../services/avatar-state';
 import { AuthService } from '../../services/auth';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, NgIf],
+  imports: [RouterLink, RouterLinkActive, NgIf],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
